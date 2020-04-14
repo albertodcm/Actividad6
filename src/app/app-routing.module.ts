e273997dbc9c051304250d18ce2041fd00843f5e
Alberto Cruz <albertocruzmendoza@outlook.com>
2020-04-13 17:22:36 -0700

    creacion de las paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,6 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
     path: 'menu',
     loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
   }
